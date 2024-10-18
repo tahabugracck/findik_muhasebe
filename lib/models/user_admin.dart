a99de0c74615506a855eae6c95a8f4e9e9be5c61
Taha Bugra Cicek <tahabugracck@gmail.com>
2024-10-18 18:24:23 +0300

    update user_admin.dart

---
+++ lib/models/user_admin.dart
@@ -0,0 +1,37 @@
+import 'package:mongo_dart/mongo_dart.dart';
+
+class UserAdminModel {
+  ObjectId id;
+  String name;
+  String password;
+  bool admin;
+  String username;
+
+  UserAdminModel({
+    required this.id,
+    required this.name,
+    required this.password,
+    required this.admin,
+    required this.username,
+  });
+
+  factory UserAdminModel.fromJson(Map<String, dynamic> json) {
+    return UserAdminModel(
+      id: json['_id'],
+      name: json['name'],
+      password: json['password'],
+      admin: json['admin'],
+      username: json['username'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      '_id': id,
+      'name': name,
+      'password': password,
+      'admin': admin,
+      'username': username,
+    };
+  }
+}
